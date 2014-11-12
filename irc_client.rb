2d73b643a24d7062f989b8a6e084ee4f88d7e61e
himrock922 <himrock922@dp117-031.jaist.ac.jp>
2014-11-13 00:51:23 +0900

    update

--- irc_client.rb
+++ irc_client.rb
@@ -313,7 +313,7 @@ Signal.trap(:INT) {
 				when 'RANDOM_TAKO'
 					s_nick = msg.split[6]
 					s_app_tmp  = msg.split[7]
-					s_app = s_app_tmp.split(/\\/)
+					s_app = s_app_tmp
 					p s_app
 				@@db.execute("#{@@app_select} where tako_app = ?", s_app) do |row|
 					p row
@@ -394,7 +394,7 @@ Signal.trap(:INT) {
 					end
 					# such channel send of infomation using of ikagent choose algorithm 
 					for key in @@channel_stable do
-						msg = %( QUERY RANDOM_TAKO "#{@@nick}" "#{select_app}") if @@algo == "1"
+						msg = %( QUERY RANDOM_TAKO #{@@nick} "#{select_app}") if @@algo == "1"
 						@@irc.privmsg "#{key}", "#{msg}" if @@algo == "1"
 					end
 					################################
