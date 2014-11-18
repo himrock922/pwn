584387bb5c86827c95330834682bd969d18ce174
himrock922 <himrock922@dp117-031.jaist.ac.jp>
2014-11-18 13:57:14 +0900

    Ikagent 0.1.2-CURRENT

--- random_tako_query.rb
+++ random_tako_query.rb
@@ -4,19 +4,28 @@ RandomTako module
 module RandomTakoQuery
 	def random_tako_query(irc, db, app_select, tako_select, nick, channel_stable)
 		select_tako = ""
-		select_app  = ""
+		select_app  = Array.new
+		join_app    = ""
 		db.execute("#{tako_select} order by random()") do |row|
 			select_tako = row[0]
 			break
 		end
-
-		db.execute("#{app_select} where tako_id = ? order by random()", select_tako)  do |row|
-			select_app =  row[1]
-			break
+		
+		i = 0
+		db.execute("#{app_select} where tako_id = ?", select_tako)  do |row|
+			select_app[i] =  row[1]
+			i += 1
 		end
+
+		i = 0
+		while select_app[i] != nil
+			join_app += "#{select_app[i]}"
+			i += 1
+		end
+			
 		# such channel send of infomation using of ikagent choose algorithm 
+		msg = " QUERY RANDOM_TAKO #{nick} #{join_tako}" 
 		for key in channel_stable do
-			msg = " QUERY RANDOM_TAKO #{nick} #{select_app}" 
 			irc.privmsg "#{key}", "#{msg}" 
 		end
 	end
