052ddf4c8515dbcce4dc904c76a3a62358a38978
himrock922 <himrock922@gmail.com>
2014-11-21 04:03:07 +0900

    Ikagent 0.1.4-CURRENT

--- irc_client.rb
+++ irc_client.rb
@@ -303,7 +303,7 @@ Signal.trap(:INT) {
 				s_nick = msg.split[6]
 				case algo
 				when 'RANDOM_TAKO'
-					s_app = msg.split[7]
+					s_app = msg.split[8]
 					s_app.encode!("UTF-8")
 					IRC::random_tako_replay(@@irc, @@db, @@app_select, @@tako_select, @@nick, @@ip, s_nick, s_app)  
 				when 'COMMON_APP'
