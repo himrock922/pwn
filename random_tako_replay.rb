7e2b7befb6a64238875e6645b625635efc927065
himrock922 <himrock922@gmail.com>
2014-11-20 20:02:21 +0900

    Ikagent 0.1.4-RELEASE

--- random_tako_replay.rb
+++ random_tako_replay.rb
@@ -8,24 +8,24 @@ module RandomTakoReplay
 		select_mac = ""
 		join_app   = ""
 		msg        = ""
-		row        = Array.new
 		####################################
 
 		# selection tako for query qpp
-		row = db.execute("#{app_select} where tako_app = ? order by random()", s_app)
-		if row.empty? != true
+		db.execute("#{app_select} where tako_app = ? order by random()", s_app) do |row|
+		if row.empty? == true
 			return
 		end
-
 		select_id = row[0]
+		end
 		##################################################
 		db.execute("#{app_select} where tako_id = ?", select_id) do |row|
 			join_app += "#{row[1]} "
 		end
 
 		# replay of selected tako information
-		row = db.execute("#{tako_select} where tako_id = ?", select_id)
-		select_mac = row[1]
+		db.execute("#{tako_select} where tako_id = ?", select_id) do |row|
+			select_mac = row[1]
+		end
 
 		msg = " REPLAY RANDOM_TAKO #{nick} #{ip} #{select_id} #{select_mac} #{join_app}"
 		irc.privmsg "#{s_nick}", "#{msg}"
