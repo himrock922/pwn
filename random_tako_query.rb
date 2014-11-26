d71aa76cf50d783b8cba57611a3a5fecc2199a53
himrock922 <himrock922@dhcp-is32e0-48.jaist.ac.jp>
2014-11-27 06:07:34 +0900

    Ikagent 0.1.5-CURRENT

--- random_tako_query.rb
+++ random_tako_query.rb
@@ -14,28 +14,31 @@ module RandomTakoQuery
 		row = db.execute("select tako_app from APP_List where tako_id = ? order by random()", select_tako[0])
 		select_app = row[0]
 
-		row = db.execute("#{cso_select} where tako_app = ? order by random()", select_app[0])
-		
-		if row.empty? == false
-			sow = db.execute("select * from CacheTako left outer join CacheSelectOne on CacheTako.tako_id = CacheSelectOne.tako_id where CacheSelectOne.tako_id = ?", row[0])
-			tow = db.execute("select * from Cache left outer join CacheTako on Cache.ikagent_ip = CacheTako.ikagent_ip where CacheTako.ikagent_ip = ?", sow[0]) 
-			line = output.gets.chomp
-			if line == "\"Timeout!\""
-				print "\r\n"
-				p "*************************"
-				p "****party tako fixed!****"
-				p "*************************"
-				input.puts "#{tow[0]}, #{tow[1]}, #{sow[1]} #{sow[2]} #{row[1]}"
-				print "#{tow[0]}, #{tow[1]}, #{sow[1]} #{sow[2]} #{row[1]}\n"
-				db.execute("delete from CacheSelectOne where tako_id = ?", row[0])
-				db.execute("delete from CacheTako where ikagent_ip = ?", sow[0])
-				db.execute("delete from Cache where ikagent_ip = ?", sow[0])
-				return
+		db.execute("#{cso_select} where tako_app = ? order by random()", select_app[0])	do |row|	
+			if row.empty? == false
+				db.execute("select * from CacheTako left outer join CacheSelectOne on CacheTako.tako_id = CacheSelectOne.tako_id where CacheSelectOne.tako_id = ?", row[0]) do |sow|
+					db.execute("select * from Cache left outer join CacheTako on Cache.ikagent_ip = CacheTako.ikagent_ip where CacheTako.ikagent_ip = ?", sow[0]) do |tow|
+						line = output.gets.chomp
+						if line == "\"Timeout!\""
+							print "\r\n"
+							p "*************************"
+							p "****party tako fixed!****"
+							p "*************************"
+							input.puts "#{tow[0]}, #{tow[1]}, #{sow[1]} #{sow[2]} #{row[1]}"
+							print "#{tow[0]}, #{tow[1]}, #{sow[1]} #{sow[2]} #{row[1]}\n"
+							db.execute("delete from CacheSelectOne where tako_id = ?", row[0])
+							db.execute("delete from CacheTako where ikagent_ip = ?", sow[0])
+							db.execute("delete from Cache where ikagent_ip = ?", sow[0])
+							return
+						else
+							return
+						end
+					end
+				end
 			else
-				return
+				break
 			end
 		end
-
 		# such channel send of infomation using of ikagent choose algorithm 
 			msg = " QUERY RANDOM_TAKO #{nick} #{ip} #{select_app[0]}" 
 			for key in channel_stable do
