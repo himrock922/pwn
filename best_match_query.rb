f0d3c30733543bb26a0206671e9a3d1dfa789a50
himrock922 <himrock922@dp117-031.jaist.ac.jp>
2014-11-27 23:17:43 +0900

    Ikagent 0.1.6-CURRENT

--- best_match_query.rb
+++ best_match_query.rb
@@ -3,21 +3,11 @@ RandomTako module
 =end
 module BestMatchQuery
 	def best_match_query(irc, db, channel_stable, app_select, nick, ip, tako_id)
-		
-		select_app = Array.new
 		join_app   = ""
 		msg        = ""
-		i = 0
 
 		db.execute("#{app_select} where tako_id = ?", tako_id) do |row|
-			select_app[i] = row[1]
-			i += 1
-		end
-
-		i = 0
-		while select_app [i] != nil
-			join_app += "#{select_app[i] }"
-			i += 1
+			join_app += "#{row[1]} "
 		end
 
 		# such channel send of infomation using of ikagent choose algorithm 
