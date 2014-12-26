dc3cf747779d6a11d2a145d98329c091850d1458
himrock922 <himrock922@gmail.com>
2014-12-27 04:12:46 +0900

    Ikagent 0.2.7-CURRENT

--- random_tako.rb
+++ random_tako.rb
@@ -6,29 +6,37 @@ module RandomTako
 		select_tako = ""
 		select_app  = ""
 		
-
+		db.transaction
 		row = db.execute("select tako_id from TAKO_List order by random()") 
 		
-		return if row.empty? == true
+		if row.empty? == true
+			db.commit
+			return
+		end
+
 		select_tako = row[0]
 
 		row = db.execute("select tako_app from APP_List where tako_id = ? order by random()", select_tako[0])
+
 		select_app = row[0]
 
-		db.execute("#{cso_select} where tako_app = ? order by random()", select_app[0])	do |row|	
-			if row.empty? == true
-				return
-			else
-			db.execute("select * from CacheTako where tako_id = ?", row[0]) do |sow|
-				db.execute("select * from Cache where ikagent_id = ?", sow[0]) do |tow|
-					print "\r\n"
-					p "*************************"
-					p "****party tako fixed!****"
-					p "*************************"
-					print "#{tow[0]}, #{tow[1]}, #{sow[1]} #{sow[2]} #{row[1]}\n"
-					end
-				end
-			end
+
+		row = db.execute("#{cso_select} where tako_app = ? order by random()", select_app[0])	
+		if row.empty? == true
+			db.commit
+			return
 		end
+		
+		row.each do |result|
+			
+			sow = db.get_first_row("select tako_mac from CacheTako where tako_id = ?", result[1])
+			tow = db.get_first_row("select ikagent_ip from Cache where ikagent_id = ?", result[0])
+			print "\r\n"
+			p "*************************"
+			p "****party tako fixed!****"
+			p "*************************"
+			print "#{result[0]}, #{tow[0]}, #{result[1]} #{sow[0]} #{result[2]}\n"
+		end
+		db.commit
 	end
 end
