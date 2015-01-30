edb2f3dbf5af24652ee3b3aaa07994d00fae1867
himrock922 <himrock922@gmail.com>
2015-01-31 06:11:07 +0900

    update

--- random_app_query.rb
+++ random_app_query.rb
@@ -11,17 +11,18 @@ module RandomAppQuery
 		db.transaction
 		row = db.get_first_row("select tako_app from APP_List where tako_id = ? order by random()", tako_id)
 		select_app = row[0]
-		p select_app
 		pow = db.execute("#{cso_select} where tako_app = ? order by random()", pow)
 		if pow.empty? == false
 			pow.each do |result|
-			sow = db.get_first_row("select tako_mac from CacheTako where tako_id = ?", result[1])
-			tow = db.get_first_row("select ikagent_ip from Cache where ikagent_id = ?", result[0])
-			line = output.gets.chomp
+				sow = db.get_first_row("select tako_mac from CacheTako where tako_id = ?", result[1])
+				tow = db.get_first_row("select ikagent_ip from Cache where ikagent_id = ?", result[0])
+				line = output.gets.chomp
 				print "\r\n"
 				p "*************************"
 				p "****party tako fixed!****"
 				p "*************************"
+				print "#{result[0]}, #{tow[0]}, #{result[1]} #{sow[0]}\n"
+				
 				if line == "\"Timeout!\""
 					input.puts "#{result[0]}, #{tow[0]}, #{result[1]} #{sow[0]}"
 					print "#{result[0]}, #{tow[0]}, #{result[1]} #{sow[0]}\n"
@@ -31,15 +32,14 @@ module RandomAppQuery
 					if uow.empty? == true
 						db.execute("delete from Cache where ikagent_id = ?", result[0])
 					end
-				else
-					print "#{result[0]}, #{tow[0]}, #{result[1]} #{sow[0]}\n"
 				end
 			end
+			db.commit
+			db.execute("vacuum")
 			return
 		end
 		# such channel send of infomation using of ikagent choose algorithm 
 		db.commit
-		db.execute("vacuum")
 
 		msg = " QUERY RANDOM_APP #{nick} #{tako_id} #{tako_mac} #{select_app}" 
 		return msg
